cecba9c8011c1644db79e9b39be3d62b8367f92f
lsoavneduy <43095273+lsoavneduy@users.noreply.github.com>
2018-10-19 02:00:56 +0800

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -14,11 +14,10 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-    {
-        let command = message.content.split(" ")[0];
-        command = command.slice(prefix.length);
-        let args = message.content.split(" ").slice(1);
-    }
+
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    let args = message.content.split(" ").slice(1);
 
     if (message.author.bot) return;
 
